65e748c6c5ee614775754962f824ed870d3a5ae7
ntphuong163 <thanhphuong.its@gmail.com>
2019-03-13 20:45:48 +0700

    fixed converting data function and appending indices_df and component_df bugs

---
+++ src/settings.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Project: ForecastingStockPrice_thesis
+# Created at: 01:37
+
+__author__ = 'phuongnt18'
+__email__ = 'phuongnt18@vng.com.vn'
+
+# time interval in a minute for scraping data
+SCRAPING_TIME = 20
+
+# time to sleep while scraping
+SLEEP_TIME = 3
+
+# Database host and name
+HOST = 'mongodb://localhost:27017/'
+DATABASE = 'StockDB'
+IndColl = 'indicesCollection'
+CompoColl = 'ComponentsCollection'
