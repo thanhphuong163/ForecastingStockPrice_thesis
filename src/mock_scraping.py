01ff6471e319263b1642bd3516a2390acb933a5b
ntphuong163 <thanhphuong.its@gmail.com>
2019-03-22 22:03:25 +0700

    Added code Mock scraping

--- src/mock_scraping.py
+++ src/mock_scraping.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 # Project: ForecastingStockPrice_thesis
 # Created at: 21:10
-import time
-
+import pandas as pd
 from pymongo import MongoClient
 
-from src.settings import DATABASE, HOST, IndColl
+from src.settings import DATABASE, HOST, IndColl, DATA_PATH
 
 __author__ = 'phuongnt18'
 __email__ = 'phuongnt18@vng.com.vn'
@@ -13,16 +12,15 @@ __email__ = 'phuongnt18@vng.com.vn'
 mockColl = 'MockCollection'
 date_fmt = '%Y-%m-%d %H:%M:%S.%f'
 
+
 if __name__ == '__main__':
 	client = MongoClient(HOST)
 	database = client.get_database(DATABASE)
 	mock_collection = database[mockColl]
 	indices_collection = database[IndColl]
-	start = 1553221822
-	end = 1553244511
 
-	docs = indices_collection.find({'time': {'$gte': start, '$lt': end}, 'name': 'VN 30 (VNI30)'})
-	for doc in docs:
-		print(doc)
-		mock_collection.insert_one(doc)
-		time.sleep(10)
+	df = pd.read_csv(DATA_PATH + 'vn30_20190322.csv')
+	df = df.drop('Unnamed: 0', axis=1)
+	for i in df.index:
+		row = dict(df.loc[i, :])
+		mock_collection.insert_one(row)
